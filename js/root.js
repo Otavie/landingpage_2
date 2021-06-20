57681410cfb0b65bc184f32274caa2fd8f91470f
Otavie <82145893+Otavie@users.noreply.github.com>
2021-06-20 15:11:34 +0100

    First Commit
    Landing page with only CSS and HTML - Few JavaScript and JQuery

---
+++ js/root.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    var typed = new Typed(".typing", {
+        strings: ["Developer", "Programmer"],
+        typeSpeed: 100,
+        backSpeed: 50,
+        loop: true
+    });
+    
+});
